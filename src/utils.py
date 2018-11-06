44742caff735591eb4b3d973b324030344b4bd24
Antoine Dozois <dozois.a@gmail.com>
2018-11-06 13:02:02 -0500

    Adding utils function for main script

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,28 @@
+import pandas as pd
+import numpy as np
 
 
 def get_split_line(line):
+    line = line.replace(" ","")
     return line.replace('\n', '').split(',')
+
+
+def read_data_csv(file_path):
+    data = pd.read_csv(file_path)
+    return data
+
+
+def extract_data_from_categorical_feature(data, feature, value):
+    return data.loc[data[feature] == value]
+
+
+def create_histogram_from_feature(data, feature, bins=100):
+    return np.histogram(data[feature], bins=bins)
+
+
+def calculate_column_width(bin_edges):
+    return np.min(bin_edges[1:] - bin_edges[:-1])
+
+
+def bar_coordinate(bin_edges, factor=1.0):
+    return (bin_edges[1:] + bin_edges[:-1]) * factor
