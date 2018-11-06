6327365b6bab5044e465f2376aa1b6a1a8a3c340
Antoine Dozois <dozois.a@gmail.com>
2018-11-06 13:04:58 -0500

    New main with all the new function

--- src/main.py
+++ src/main.py
@@ -1,42 +1,47 @@
-from src.utils import get_split_line
-import numpy as np
+from src.utils import get_split_line, read_data_csv, extract_data_from_categorical_feature, create_histogram_from_feature
+from src.utils import bar_coordinate, calculate_column_width
 import matplotlib.pyplot as plt
+import logging
 
 
 def main():
-    file = open('../data/data.csv', 'r')
-    ListOfAllLines = []
-    for line in file:
-        ListOfAllLines.append(line)
-    x = []
-    y = []
-    z = []
-    w = []
-    for line in ListOfAllLines:
-        if line[0] == 'M':
-            split_line = get_split_line(line)
-            x.append(float(split_line[1]))
-            y.append(float(split_line[2]))
-        elif line[0] == 'F':
-            split_line = get_split_line(line)
-            z.append(float(split_line[1]))
-            w.append(float(split_line[2]))
-    counts_x, bin_edges_x = np.histogram(x, bins=100)
-    counts_y, bin_edges_y = np.histogram(y, bins=100)
-    counts_z, bin_edges_z = np.histogram(z, bins=100)
-    counts_w, bin_edges_w = np.histogram(w, bins=100)
-    column_width = np.min(bin_edges_x[1:] - bin_edges_x[:-1])
-    plt.bar(0.5*(bin_edges_x[1:] +bin_edges_x[:-1]),  counts_x, label='men\'s age', alpha=0.5, width=column_width)
-    column_width = np.min(bin_edges_z[1:] - bin_edges_z[:-1])
-    plt.bar(0.5*(bin_edges_z[1:] +bin_edges_z[:-1]),  counts_z, label='women\'s age', alpha=0.5, width=column_width)
+    file_path = '../data/data.csv'
+    try:
+        dataset = read_data_csv(file_path)
+    except Exception as err:
+        print("Couldn't open file")
+        logging.exception(err)
+        return
+
+    men_data = extract_data_from_categorical_feature(dataset, "gender", "M")
+    women_data = extract_data_from_categorical_feature(dataset, "gender", "F")
+
+    counts_men_age, bin_edges_men_age = create_histogram_from_feature(men_data, 'age')
+    counts_men_height, bin_edges_men_height = create_histogram_from_feature(men_data, 'height')
+    counts_women_age, bin_edges_women_age = create_histogram_from_feature(women_data, 'age')
+    counts_women_height, bin_edges_women_height = create_histogram_from_feature(women_data, 'height')
+
+    plt.bar(bar_coordinate(bin_edges_men_age, 0.5), counts_men_age, label='men\'s age', alpha=0.5,
+            width=calculate_column_width(bin_edges_men_age))
+    plt.bar(bar_coordinate(bin_edges_women_age, 0.5), counts_women_age, label='women\'s age', alpha=0.5,
+            width=calculate_column_width(bin_edges_women_age))
     plt.legend(loc=0)
+    plt.title('Age distribution of the individuals')
+    plt.xlabel('Age of individuals')
+    plt.ylabel('Frequency')
     plt.savefig('age_distribution.png')
     plt.close()
-    column_width = np.min(bin_edges_y[1:] - bin_edges_y[:-1])
-    plt.bar(0.5*(bin_edges_y[1:] +bin_edges_y[:-1]),  counts_y, label='men\'s height', alpha=0.5, width=column_width)
-    column_width = np.min(bin_edges_w[1:] - bin_edges_w[:-1])
-    plt.bar(0.5*(bin_edges_w[1:] +bin_edges_w[:-1]),  counts_w, label='women\'s height', alpha=0.5, width=column_width)
+
+    plt.bar(bar_coordinate(bin_edges_men_height, 0.5), counts_men_height, label='men\'s height', alpha=0.5,
+            width=calculate_column_width(bin_edges_men_height))
+    plt.bar(bar_coordinate(bin_edges_women_height, 0.5), counts_women_height, label='women\'s height', alpha=0.5,
+            width=calculate_column_width(bin_edges_women_height))
     plt.legend(loc=0)
+    plt.title('Height distribution of the individuals')
+    plt.xlabel('height of individuals (m)')
+    plt.ylabel('Frequency')
     plt.savefig('height_distribution.png')
-    file.close()
-main()
+
+
+if __name__=="__main__":
+    main()
